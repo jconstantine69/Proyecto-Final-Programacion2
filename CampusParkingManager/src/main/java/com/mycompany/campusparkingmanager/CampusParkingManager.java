ae567d903fec8085697bd87e24ed4ec495b9410f
hellblazer <valentinors2005@outlook.com>
2025-10-15 20:42:21 -0600

    feat(app): inicializa proyecto con JFrame básico

---
+++ CampusParkingManager/src/main/java/com/mycompany/campusparkingmanager/CampusParkingManager.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.campusparkingmanager;
+
+/**
+ *
+ * @author valen
+ */
+public class CampusParkingManager {
+
+    public static void main(String[] args) {
+        Application app = new Application();
+        app.setVisible(true);
+    }
+}
